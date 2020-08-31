9e893a17df9ff8fb10b234bb84e6deaa8fce4edb
saviosabino <saviopp@gmail.com>
2020-08-30 21:26:44 -0300

    Refactored Map Impl to abstract ID generation. Closes #23. lecture 84

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2441302184792880252L;
+
 	public Long getId() {
 		return id;
 	}
