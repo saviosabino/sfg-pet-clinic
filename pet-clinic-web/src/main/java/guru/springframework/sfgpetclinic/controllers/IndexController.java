d5dcbf517d18b0e0a71b4d98ee2537c00f43fc0b
saviosabino <saviopp@gmail.com>
2020-09-03 09:52:05 -0300

    Fixed Broken Links. Closes #45. lecture 126

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,8 @@ public class IndexController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("/oups")
+	public String oupsHandler() { return "notimplemented"; }
+	
 }
