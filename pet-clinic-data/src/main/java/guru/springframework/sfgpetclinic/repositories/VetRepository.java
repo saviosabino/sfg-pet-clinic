28b78068f3b5fe12980ea4eebaaf900988aee975
saviosabino <saviopp@gmail.com>
2020-09-08 10:46:22 -0300

    added vet spring data jpa. closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import guru.springframework.sfgpetclinic.model.Vet;
 
 public interface VetRepository extends CrudRepository<Vet, Long>  {
-
+	Vet findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+public class VetSDJpaService implements VetService{
+
+	private final VetRepository vetRepository;
+	
+	public VetSDJpaService(VetRepository vetRepository) {
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+		vetRepository.findAll().forEach(vets::add);
+		return null;
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return vetRepository.save(object);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		vetRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+	@Override
+	public Vet findByLastName(String lastName) {
+		return vetRepository.findByLastName(lastName);
+	}
+	
+}
