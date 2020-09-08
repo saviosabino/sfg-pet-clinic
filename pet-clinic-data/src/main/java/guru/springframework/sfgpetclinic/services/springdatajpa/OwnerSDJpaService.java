795a825a8ff1cf219f7c8c638ee7d92045c236da
saviosabino <saviopp@gmail.com>
2020-09-08 10:36:16 -0300

    added spring data jpa service for owners. closes #40. lecture 150

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+public class OwnerSDJpaService implements OwnerService{
+	
+	private final OwnerRepository ownerRepository;
+	
+	public OwnerSDJpaService(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<>();
+		ownerRepository.findAll().forEach(owners::add);
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return ownerRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownerRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return ownerRepository.findByLastName(lastName);
+	}
+	
+}
