eb65bae51c3115712396d93d4f995fe1819a1f68
saviosabino <saviopp@gmail.com>
2020-09-07 22:05:41 -0300

    updated vet to use JPA. closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -9,7 +9,7 @@ public class Vet extends Person{
 
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
-			inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+			inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialties  = new HashSet<>();
 
 	public Set<Specialty> getSpecialties() { return specialties; }
