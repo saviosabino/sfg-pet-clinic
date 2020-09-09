dcc74c241695fc8bca95e135c5ef613c1e8df46b
saviosabino <saviopp@gmail.com>
2020-09-09 12:39:31 -0300

    Added Project Lombok to project, closes #51. lecture 167

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,16 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class PetType extends BaseEntity {
-
 	
 	private String name;
 
-	public String getName() { return name; }
-
-	public void setName(String name) { this.name = name; }
-	
-	
 }
