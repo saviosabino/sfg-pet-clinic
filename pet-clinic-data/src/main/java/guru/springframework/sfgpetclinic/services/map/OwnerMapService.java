2593dc5ea88988c1593e804fbb24f02df9a3c558
saviosabino <saviopp@gmail.com>
2020-09-16 00:39:20 -0300

    implement findByLastName and test all methods of ownerMapService. closes #49. lecture 184.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -73,8 +73,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long>
 
 	@Override
 	public Owner findByLastName(String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.findAll()
+			.stream()
+			.filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+			.findFirst().orElse(null);
 	}
 
 }
